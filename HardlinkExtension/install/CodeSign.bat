8d371204a42bc8a9a32f8ca82e42cc505cf63008
Hermann Schinagl <hermann@schinagl.priv.at>
2019-10-05 12:36:48 +0200

    Add time server during code signing

--- HardlinkExtension/install/CodeSign.bat
+++ HardlinkExtension/install/CodeSign.bat
@@ -3,22 +3,16 @@ cd %~dp0
 call ..\Settings.bat
 
 @set CERTIFICATE=..\..\Shared\certificate\LinkShellextension.pfx
+@set TIME_SERVER="http://timestamp.entrust.net/TSS/RFC3161sha2TS"
 @set SIGNTOOL=..\..\tools\signtool.exe
 
-%SIGNTOOL% sign /f %CERTIFICATE% /v /i "Hermann Schinagl" /d "Link Shellextension" /du "http://schinagl.priv.at" ..\..\Bin\x64\Release\HardlinkShellExt.dll
-%SIGNTOOL% sign /f %CERTIFICATE% /v /i "Hermann Schinagl" /d "Link Shellextension" /du "http://schinagl.priv.at" ..\..\Bin\win32\Release\HardlinkShellExt.dll
-
-%SIGNTOOL% sign /f %CERTIFICATE% /v /i "Hermann Schinagl" /d "Link Shellextension" /du "http://schinagl.priv.at" ..\..\Bin\x64\Release\LSEUacHelper.exe
-%SIGNTOOL% sign /f %CERTIFICATE% /v /i "Hermann Schinagl" /d "Link Shellextension" /du "http://schinagl.priv.at" ..\..\Bin\win32\Release\LSEUacHelper.exe
-
-%SIGNTOOL% sign /f %CERTIFICATE% /v /i "Hermann Schinagl" /d "Link Shellextension" /du "http://schinagl.priv.at" ..\..\Bin\x64\Release\LSEConfig.exe
-%SIGNTOOL% sign /f %CERTIFICATE% /v /i "Hermann Schinagl" /d "Link Shellextension" /du "http://schinagl.priv.at" ..\..\Bin\win32\Release\LSEConfig.exe
-
+set FILES_TO_SIGN=..\..\Bin\x64\Release\HardlinkShellExt.dll ..\..\Bin\win32\Release\HardlinkShellExt.dll ..\..\Bin\x64\Release\LSEUacHelper.exe ..\..\Bin\win32\Release\LSEUacHelper.exe ..\..\Bin\win32\Release\LSEConfig.exe ..\..\Bin\x64\Release\LSEConfig.exe
+%SIGNTOOL% sign /v /fd sha256 /td sha256 /f  %CERTIFICATE% /tr %TIME_SERVER% %FILES_TO_SIGN%
 
 call CreateInstall.bat
 
-%SIGNTOOL% sign /f %CERTIFICATE% /v /i "Hermann Schinagl" /d "Link Shellextension" /du "http://schinagl.priv.at" ..\..\Media\HardLinkShellExt_X64.exe
-%SIGNTOOL% sign /f %CERTIFICATE% /v /i "Hermann Schinagl" /d "Link Shellextension" /du "http://schinagl.priv.at" ..\..\Media\HardLinkShellExt_win32.exe
+set FILES_TO_SIGN=..\..\Media\HardLinkShellExt_X64.exe ..\..\Media\HardLinkShellExt_win32.exe
+%SIGNTOOL% sign /v /fd sha256 /td sha256 /f  %CERTIFICATE% /tr %TIME_SERVER% %FILES_TO_SIGN%
 
 
 
