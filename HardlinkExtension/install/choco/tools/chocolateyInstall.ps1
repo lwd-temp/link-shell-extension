05fe519fed3851e8d41d24bfca9288034e423f02
Hermann Schinagl <hermann@schinagl.priv.at>
2019-11-17 10:25:03 +0100

    Linkshell Extension Choco Package working

--- HardlinkExtension/install/choco/tools/chocolateyInstall.ps1
+++ HardlinkExtension/install/choco/tools/chocolateyInstall.ps1
@@ -2,10 +2,10 @@
 $installerType = 'exe'
 $silentArgs = '/S'
 $url = 'https://schinagl.priv.at/nt/hardlinkshellext/HardLinkShellExt_win32.exe'
-$checksum = '74d21fb9940df5af6d7a661cf6f306e9444a885f5bd367afce92847046214a6f'
+$checksum = '92537df7c0e262d33959b56f9996d830a797725099ecde51d0db92427c6b04b3'
 $checksumType = 'sha256'
 $url64 = 'https://schinagl.priv.at/nt/hardlinkshellext/HardLinkShellExt_X64.exe'
-$checksum64 = 'f148bb3954b83b6ee269c17fa650f3a0248ba686239b1cff21ff17825b046e23'
+$checksum64 = '46b98cb6c7864af174edc816545f383c5e786c11e55d15ce9d354ff7a0950b78'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
