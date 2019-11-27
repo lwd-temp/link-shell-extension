b5ce946f8632dddc60bb22abc17c5b62614b676b
Hermann Schinagl <hermann@schinagl.priv.at>
2019-11-27 21:41:20 +0100

    The 1103 commit

--- BuildWorld.bat
+++ BuildWorld.bat
@@ -20,7 +20,7 @@ REM
 set MAJOR_DUPEMERGE_VERSION=1
 set MINOR_DUPEMERGE_VERSION=1
 set PATCH_DUPEMERGE_VERSION=0
-set HOTFIX_DUPEMERGE_VERSION=2
+set HOTFIX_DUPEMERGE_VERSION=3
 
 @echo generating Version info
 REM Generate version info for Shell Extension
