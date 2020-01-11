701fb30f407eebcd8049d830d2c82692e4d3e721
Hermann Schinagl <hermann@schinagl.priv.at>
2020-01-11 20:13:35 +0100

    The Hashes

--- HardlinkExtension/install/choco/tools/chocolateyInstall.ps1
+++ HardlinkExtension/install/choco/tools/chocolateyInstall.ps1
@@ -1,11 +1,11 @@
 ﻿$packageName = 'Link Shellextension'
 $installerType = 'exe'
 $silentArgs = '/S /noredist'
-$url = 'https://schinagl.priv.at/nt/hardlinkshellext/save/3931/HardLinkShellExt_win32.exe'
-$checksum = '5975120458cf4acecc0b98057abf10e75a78ef1d5bc2e10b331847a76e508c4f'
+$url = 'https://schinagl.priv.at/nt/hardlinkshellext/save/3932/HardLinkShellExt_win32.exe'
+$checksum = '0db6aeeb2e64e76e970b0d50ce6f1ea0e065d717ccf4173e34ce29f2a4bc04f4'
 $checksumType = 'sha256'
-$url64 = 'https://schinagl.priv.at/nt/hardlinkshellext/save/3931/HardLinkShellExt_X64.exe'
-$checksum64 = '6b72449277601eb7a0c15992edcff7d835d7f99836f0d02a2687856a994a3654'
+$url64 = 'https://schinagl.priv.at/nt/hardlinkshellext/save/3932/HardLinkShellExt_X64.exe'
+$checksum64 = '328b9ce1257a2e148faa4a611a11f48e352cbb663dd9c1e9240e1a4f9d72ab47'
 $checksumType64 = 'sha256'
 $validExitCodes = @(0)
 
--- ln/choco/tools/chocolateyInstall.ps1
+++ ln/choco/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ln'
-$url = 'https://schinagl.priv.at/nt/ln/save/2924/ln.zip'
+$url = 'https://schinagl.priv.at/nt/ln/save/2925/ln.zip'
 $checksum = 'f83e892222610113c5a6eaf731e1f147bbea1dcc8d806d614b67db2a482a4d5f'
 $checksumType = 'sha256'
-$url64 = 'https://schinagl.priv.at/nt/ln/save/2924/ln64.zip'
+$url64 = 'https://schinagl.priv.at/nt/ln/save/2925/ln64.zip'
 $checksum64 = 'f3b5d0603a1712c4f8c361407a5c4afac8d81b47f86042684228765f05d87e60'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
