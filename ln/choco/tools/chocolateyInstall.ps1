f172bbbc480117c2d67f3297095c95b39e436ef3
Hermann Schinagl <hermann@schinagl.priv.at>
2021-01-28 20:09:31 +0100

    The 2932 choco hashes

--- ln/choco/tools/chocolateyInstall.ps1
+++ ln/choco/tools/chocolateyInstall.ps1
@@ -1,9 +1,9 @@
 ﻿$packageName = 'ln'
-$url = 'https://schinagl.priv.at/nt/ln/save/2931/ln.zip'
-$checksum = 'a229fcb3c0bc403cc3d72cabaeb3d89cf8b2b99f03d19c6ab5147bc7b3aa561d'
+$url = 'https://schinagl.priv.at/nt/ln/save/2932/ln.zip'
+$checksum = 'e0483b86c627f05f6fbb3fb9f122b7e8a82d1c38352f1e6bf09b65611246e64a'
 $checksumType = 'sha256'
-$url64 = 'https://schinagl.priv.at/nt/ln/save/2931/ln64.zip'
-$checksum64 = '9accc61ad084767b99f79e6548da94c3d844775f5e618ba9a04a6192d34f5a32'
+$url64 = 'https://schinagl.priv.at/nt/ln/save/2932/ln64.zip'
+$checksum64 = '109c4afba7320a580f8c39f811bfaa081e6f40e3f746bc99ba511338ee092b8e'
 $checksumType64 = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
